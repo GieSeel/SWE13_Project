d60c9fc5cd78a92b21d7fbb435228c5d79767d52
Flopper2 <florian@dieseels.de>
2013-07-25 11:33:26 -0700

    using language property

--- src/de/dhbw/swe/campingplatzverwaltung/Campingplatzverwaltung.java
+++ src/de/dhbw/swe/campingplatzverwaltung/Campingplatzverwaltung.java
@@ -1,6 +1,6 @@
 package de.dhbw.swe.campingplatzverwaltung;
 
-import de.dhbw.swe.campingplatzverwaltung.common.language_mgt.LanguageMgr;
+import de.dhbw.swe.campingplatzverwaltung.common.language_mgt.*;
 
 /**
  * Insert description for Campingplatzverwaltung
@@ -10,8 +10,13 @@ import de.dhbw.swe.campingplatzverwaltung.common.language_mgt.LanguageMgr;
  */
 public class Campingplatzverwaltung {
 
+    /**   */
+    private final static LanguageMgr lm = LanguageMgr.getInstance();
+
+    private static LanguageProperties lp;
+
     public static void main(final String[] args) {
-	LanguageMgr.getInstance();
+	System.out.println(lm.get(lp.MAP));
     }
 
 }
