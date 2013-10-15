425d28a71dc5149676add6ff0ec05e8dd6c3be28
Florian Seel <florian@dieseels.de>
2013-10-15 16:39:33 +0200

    horizontal BaseFormularPanel

--- src/de/dhbw/swe/camping_site_mgt/gui_mgt/BaseFormularPanel.java
+++ src/de/dhbw/swe/camping_site_mgt/gui_mgt/BaseFormularPanel.java
@@ -17,12 +17,22 @@ public class BaseFormularPanel extends JPanel {
 	    final GridBagConstraints lpGbc = new GridBagConstraints();
 	    lpGbc.anchor = GridBagConstraints.CENTER;
 	    lpGbc.fill = GridBagConstraints.HORIZONTAL;
-	    lpGbc.weightx = 1;
-	    final JLabel label = new JLabel(string, JLabel.LEFT);
-	    // label.setPreferredSize(new Dimension(200, 30));
-	    this.add(label, lpGbc);
+
+	    if (horizontalAlignment) {
+		lpGbc.gridx = 0;
+		lpGbc.weightx = 0.5;
+		final JLabel label = new JLabel(string, JLabel.LEFT);
+		this.add(label, lpGbc);
+		lpGbc.gridy = 0;
+		lpGbc.gridx = 1;
+	    } else {
+		lpGbc.weightx = 1;
+		final JLabel label = new JLabel(string, JLabel.LEFT);
+		this.add(label, lpGbc);
+		lpGbc.gridy = 1;
+	    }
 	    lpGbc.weighty = 2;
-	    lpGbc.gridy = 1;
+
 	    this.add(c, lpGbc);
 	}
     }
@@ -31,10 +41,17 @@ public class BaseFormularPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
     public BaseFormularPanel() {
+	this(true);
+    }
+
+    public BaseFormularPanel(final Boolean horizontalAlignment) {
+	this.horizontalAlignment = horizontalAlignment;
 	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
 
     public void add(final String subject, final JComponent value) {
 	add(new LabelPanel(subject, value));
     }
+
+    private final Boolean horizontalAlignment;
 }
